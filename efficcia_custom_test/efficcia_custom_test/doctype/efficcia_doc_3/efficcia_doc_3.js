d957276a5f028f41158d19f26a3319cc874ca5b7
cegomezpy <cegomezpy@gmail.com>
2025-10-13 20:58:30 +0000

    feat/new-commit

---
+++ efficcia_custom_test/efficcia_custom_test/doctype/efficcia_doc_3/efficcia_doc_3.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, cegomezpy and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Efficcia Doc 3", {
+// 	refresh(frm) {
+
+// 	},
+// });
