e5eae6e9363496c8c9fc87034f6b5500348f37f5
cegomezpy <cegomezpy@gmail.com>
2025-10-16 19:11:51 +0000

    feat/add-efficcia-doc-4

---
+++ efficcia_custom_test/efficcia_custom_test/doctype/efficcia_doc_4/efficcia_doc_4.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, cegomezpy and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Efficcia Doc 4", {
+// 	refresh(frm) {
+
+// 	},
+// });
