a5eaee4ace99c7527475d5b02430e548a7b6e441
Lester Diaz <fsharker0810@yahoo.com>
2025-10-24 23:18:55 -0400

    Initialize setup.py for custom_app package

---
+++ custom_app/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='custom_app',
+    version='0.0.1',
+    description='My custom ERPNext/Frappe application.',
+    packages=['custom_app'],
+    include_package_data=True,
+    install_requires=[]
+)
