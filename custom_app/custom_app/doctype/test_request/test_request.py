80f37a160464081a1dc4ddb3f252d67ba2dc1b2a
Lester Diaz <fsharker0810@yahoo.com>
2025-10-24 23:26:15 -0400

    Add TestRequest document class in test_request.py

---
+++ custom_app/custom_app/doctype/test_request/test_request.py
@@ -0,0 +1,13 @@
+# Contenido de test_request.py
+
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 Deepzide and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class TestRequest(Document):
+    # Aquí puedes añadir métodos, validaciones o lógica
+    # que se ejecuta antes de guardar o al guardar el documento.
+    pass
