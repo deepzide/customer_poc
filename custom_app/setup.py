7ea84cf9825db364f6321b96ff1194ebe213e9cb
Lester Diaz <fsharker0810@yahoo.com>
2025-10-24 23:21:59 -0400

    Update setup.py with author and license information

--- custom_app/setup.py
+++ custom_app/setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+# Contenido de custom_app/setup.py
+
+from setuptools import setup, find_packages
 
 setup(
     name='custom_app',
     version='0.0.1',
-    description='My custom ERPNext/Frappe application.',
-    packages=['custom_app'],
+    description='My Custom App for ERPNext/Frappe',
+    author='Deepzide',
+    license='MIT',
+    packages=find_packages(),
     include_package_data=True,
-    install_requires=[]
+    install_requires=[],
+    zip_safe=False
 )
